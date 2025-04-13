a5662f134ca598d6cf6609b4fde4c6e63a0321f4
Nadezda <Bogdancikk@gmail.com>
2025-04-14 00:57:03 +0300

    [homework/variables]: Learn Java Variables

---
+++ src/main/java/lv/acodemy/Variables.java
@@ -0,0 +1,29 @@
+package lv.acodemy;
+
+public class Variables {
+    public static void main(String[] args) {
+        // Integer types
+        byte age = 25;
+        short year = 2025;
+        int population = 1500000;
+        long distance = 123456789L;
+
+        // Floating point types
+        float pi = 3.14f;
+        double e = 2.718281828;
+
+        // Character type
+        char grade = 'A';
+
+        // Boolean types
+        boolean isStudent = true;
+        boolean hasAccess = false;
+
+        // Strings
+        String name = "Alice";
+        String city = "Moscow";
+
+        // You can add more variables and group them by type
+    }
+}
+
